b2b5c6c62c0c0855951c9b908abbb7d30c130c62
RayaRabbani129 <arya.rabbani06@gmail.com>
2024-12-04 14:05:28 +0700

    Tahu..isi... Bakwan...

--- tugas.java
+++ tugas.java
@@ -1,7 +1,16 @@
 package kolab;
 
+import java.util.Scanner;
+
 public class tugas {
     public static void main(String[] args) {
-        System.out.println("p");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== PENCATATAN PRESTASI MAHASISWA ===");
+        System.out.println("1. Tambah Data Prestasi");
+        System.out.println("2. Tampilkan Semua Prestasi");
+        System.out.println("Analisi Prestasi Berdasarkan Jenis");
+        System.out.println("Keluar");
+        System.out.println("Pilih menu: 2");
+        System.out.println("Belum ada data prestasi");
     }
 }
